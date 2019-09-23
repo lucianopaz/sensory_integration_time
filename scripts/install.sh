1d9667191e7d74478a8fea6103f1873dd2f90245
lucianopaz <luciano.paz.neuro@gmail.com>
2019-09-23 10:52:18 +0200

    Central install script

---
+++ scripts/install.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Parse input
+usage() { echo "Usage: $0 [-n conda environment onto which install] [-d flag to indicate whether to install the package in pip development mode]" 1>&2; exit 1; }
+TARGET_ENV=$CONDA_DEFAULT_ENV
+
+while getopts ":n:d" o; do
+  case "${o}" in
+    n)
+      TARGET_ENV=${OPTARG}
+      ;;
+    d)
+      DEVELOPMENT=1
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+echo $TARGET_ENV
+
+# Get the scripts directory and the short uname
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OS=$(uname -s)
+
+# Get the conda environment path
+conda_env_root_dir=$(dirname $CONDA_PREFIX | xargs basename)
+if [[ $conda_env_root_dir == envs ]]
+then
+  CONDA_ENVS_DIR=$(dirname $CONDA_PREFIX)
+  NOT_IN_CONDA_ROOT_ENV=1
+else
+  CONDA_ENVS_DIR="$CONDA_PREFIX/envs"
+fi
+
+# Activate target conda environment
+if [[ $TARGET_ENV != $CONDA_DEFAULT_ENV ]] && [[ ! -d "$CONDA_ENVS_DIR/$TARGET_ENV" ]] && [[ $TARGET_ENV != "base" ]]
+then
+  echo "Will create a conda environment $TARGET_ENV"
+  conda create -n $TARGET_ENV -y
+fi
+
+if [[ $TARGET_ENV != $CONDA_DEFAULT_ENV ]]
+then
+  if [[ $TARGET_ENV != "base" ]]
+  then
+    TARGET_ENV_DIR=$CONDA_ENVS_DIR/$TARGET_ENV
+  else
+    TARGET_ENV_DIR=$(dirname $CONDA_ENVS_DIR)
+  fi
+else
+  TARGET_ENV_DIR=$CONDA_PREFIX
+fi
+
+# Update the environment's activation env_vars scripts
+mkdir -p $TARGET_ENV_DIR/etc/conda/activate.d
+mkdir -p $TARGET_ENV_DIR/etc/conda/deactivate.d
+
+# Check if env_vars exist
+if [[ ! -f $TARGET_ENV_DIR/etc/conda/activate.d/env_vars.sh ]]
+then
+  echo -e "#!/usr/bin/env bash\n" > $TARGET_ENV_DIR/etc/conda/activate.d/env_vars.sh
+fi
+if [[ ! -f $TARGET_ENV_DIR/etc/conda/deactivate.d/env_vars.sh ]]
+then
+  echo -e "#!/usr/bin/env bash\n" > $TARGET_ENV_DIR/etc/conda/deactivate.d/env_vars.sh
+fi
+
+# Init the activation commands
+HEADER="####### LINES ADDED BY THE INSTALLATION OF THE LEAKYINTEGRATION PACKAGE ######"
+ACTIVATION="
+_OLD_CINCLUDE_PATH=\$C_INCLUDE_PATH
+_OLD_CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH
+_OLD_LD_LIBRARY_PATH=\$LD_LIBRARY_PATH
+_OLD_DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH
+
+export C_INCLUDE_PATH=$TARGET_ENV_DIR/include/:\$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$TARGET_ENV_DIR/include/:\$CPLUS_INCLUDE_PATH
+export LD_LIBRARY_PATH=$TARGET_ENV_DIR/lib/:\$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$TARGET_ENV_DIR/lib/:\$DYLD_LIBRARY_PATH"
+DEACTIVATION="
+export C_INCLUDE_PATH=\$_OLD_CINCLUDE_PATH
+export CPLUS_INCLUDE_PATH=\$_OLD_CPLUS_INCLUDE_PATH
+export LD_LIBRARY_PATH=\$_OLD_LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=\$_OLD_DYLD_LIBRARY_PATH
+
+unset _OLD_CINCLUDE_PATH
+unset _OLD_CPLUS_INCLUDE_PATH
+unset _OLD_LD_LIBRARY_PATH
+unset _OLD_DYLD_LIBRARY_PATH"
+
+# Check if env_vars was already edited, and if not, edit
+if [[ ! $(grep -q "$HEADER" $TARGET_ENV_DIR/etc/conda/activate.d/env_vars.sh) ]]
+then
+  echo "$HEADER" >> $TARGET_ENV_DIR/etc/conda/activate.d/env_vars.sh
+  echo -e "$ACTIVATION" >> $TARGET_ENV_DIR/etc/conda/activate.d/env_vars.sh
+fi
+if [[ ! $(grep -q "$HEADER" $TARGET_ENV_DIR/etc/conda/deactivate.d/env_vars.sh) ]]
+then
+  echo "$HEADER" >> $TARGET_ENV_DIR/etc/conda/deactivate.d/env_vars.sh
+  echo -e "$DEACTIVATION" >> $TARGET_ENV_DIR/etc/conda/deactivate.d/env_vars.sh
+fi
+
+# Activate environment
+if [[ $TARGET_ENV != $CONDA_DEFAULT_ENV ]]
+then
+  echo "Activating target environment $TARGET_ENV"
+  set +x
+  eval "$(conda shell.bash hook)"
+  conda activate $TARGET_ENV
+fi
+set -x
+
+# conda install requirements
+conda install -y make gsl pip
+
+# Platform dependent installs
+# Ugly hack because we are not using conda build
+if [[ $OS == Linux ]]
+then
+  conda install -y -c anaconda gfortran_linux-64
+elif [[ $OS == Darwin ]]
+then
+   conda install -y -c conda-forge gfortran_osx-64 
+fi
+
+# Make the fortran shared library
+cd $DIR/../leakyIntegrator/src
+make shared
+
+# pip install the package
+cd $DIR/..
+pip install --upgrade pip
+pip install -r requirements.txt
+if [[ -z $DEVELOPMENT ]]
+then
+  pip install .
+else
+  pip install -e .
+fi
+
+echo "Successfully installed package!"
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 """
 Setup script for the leakyIntegrator package related to the publication ...
 It involves the use of a Makefile during the build phase, and it does so
@@ -20,74 +20,85 @@ from distutils.errors import DistutilsSetupError
 from distutils import log as distutils_logger
 
 
+conda_environment_path = os.environ["CONDA_PREFIX"]
+if os.path.dirname(conda_environment_path).endswith("envs"):
+    conda_environment = os.environ["CONDA_DEFAULT_ENV"]
+else:
+    conda_environment = None
 
-extension1 = setuptools.extension.Extension('leakyIntegrator.leaky_integral_calculator',
-                    sources = ['leakyIntegrator/src/leaky_integral_calculator.c'],
-                    libraries = ['m',
-                                 'dl',
-                                 'gsl',
-                                 'gslcblas',
-                                 'gfortran',
-                                 'quadmath'],
-                    )
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
+README_FILE = os.path.join(PROJECT_ROOT, "README.md")
+
+
+extension1 = setuptools.extension.Extension(
+    "leakyIntegrator.leaky_integral_calculator",
+    sources=["leakyIntegrator/src/leaky_integral_calculator.c"],
+    libraries=["m", "dl", "gsl", "gslcblas", "gfortran", "quadmath"],
+)
+
 
 class specialized_build_ext(build_ext, object):
     """
     Specialized builder for incgamNEG library
     
     """
+
     special_extension = extension1.name
-    
+
     def finalize_options(self):
         # The reason for this is the need to get numpy include and
         # library dirs but numpy could a priori not be available! This
         # means that you need to bootstrap numpy's installation.
         # This hackish solution was taken from
         # https://stackoverflow.com/a/21621689/7253166
-        
-        
+
         super(specialized_build_ext, self).finalize_options()
         # Prevent numpy from thinking it is still in its setup process:
         __builtins__.__NUMPY_SETUP__ = False
         import numpy
+
         self.include_dirs.append(numpy.get_include())
-    
+
     def build_extension(self, ext):
-        
-        if ext.name!=self.special_extension:
+
+        if ext.name != self.special_extension:
             # Handle unspecial extensions with the parent class' method
             super(specialized_build_ext, self).build_extension(ext)
         else:
             # Get the package build dir
-            build_py = self.get_finalized_command('build_py')
+            build_py = self.get_finalized_command("build_py")
             fullname = self.get_ext_fullname(ext.name)
-            modpath = fullname.split('.')
-            package = '.'.join(modpath[:-1])
+            modpath = fullname.split(".")
+            package = ".".join(modpath[:-1])
             package_dir = build_py.get_package_dir(package)
-            
+
             # Handle special extension
             sources = ext.sources
             if sources is None or not isinstance(sources, (list, tuple)):
                 raise DistutilsSetupError(
-                       "in 'ext_modules' option (extension '%s'), "
-                       "'sources' must be present and must be "
-                       "a list of source filenames" % ext.name)
+                    "in 'ext_modules' option (extension '%s'), "
+                    "'sources' must be present and must be "
+                    "a list of source filenames" % ext.name
+                )
             sources = list(sources)
-        
-            if len(sources)>1:
+
+            if len(sources) > 1:
                 sources_path = os.path.commonprefix(sources)
             else:
                 sources_path = os.path.dirname(sources[0])
             sources_path = os.path.realpath(sources_path)
             if not sources_path.endswith(os.path.sep):
-                sources_path+= os.path.sep
-            
+                sources_path += os.path.sep
+
             if not os.path.exists(sources_path) or not os.path.isdir(sources_path):
                 raise DistutilsSetupError(
-                       "in 'extensions' option (extension '%s'), "
-                       "the supplied 'sources' base dir "
-                       "must exist" % ext.name)
-            
+                    "in 'extensions' option (extension '%s'), "
+                    "the supplied 'sources' base dir "
+                    "must exist" % ext.name
+                )
+
             output_dir = os.path.realpath(os.path.join(self.build_lib, package_dir))
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
@@ -96,93 +107,115 @@ class specialized_build_ext(build_ext, object):
                 ext.library_dirs.append(output_dir)
             if output_dir not in ext.runtime_library_dirs:
                 ext.runtime_library_dirs.append(output_dir)
-            
+
             plat = platform.system().lower()
-            if plat.startswith('windows'):
-                shared_lib = 'incgamNEG.*'
-                static_lib = 'incgamNEG.*'
-                mv = 'move'
-            elif plat.startswith('darwin'):
-                shared_lib = 'libincgamNEG.dylib'
-                static_lib = 'libincgamNEG.a'
-                mv = 'mv'
+            if plat.startswith("windows"):
+                shared_lib = "incgamNEG.*"
+                static_lib = "incgamNEG.*"
+                mv = "move"
+            elif plat.startswith("darwin"):
+                shared_lib = "libincgamNEG.dylib"
+                static_lib = "libincgamNEG.a"
+                mv = "mv"
             else:
-                shared_lib = 'libincgamNEG.so'
-                static_lib = 'libincgamNEG.a'
-                mv = 'mv'
-            library_type = 'shared'
-            output_lib = shared_lib if library_type=='shared' else static_lib
-            ext.libraries.append(':{0}'.format(output_lib))
-            
-            shell_command = 'make {lib_type} && {mv} {source} {dest}'.format(lib_type = library_type,
-                                                                             mv = mv,
-                                                                             source = output_lib,
-                                                                             dest = os.path.join(output_dir, output_lib))
+                shared_lib = "libincgamNEG.so"
+                static_lib = "libincgamNEG.a"
+                mv = "mv"
+            library_type = "shared"
+            output_lib = shared_lib if library_type == "shared" else static_lib
+            ext.libraries.append(":{0}".format(output_lib))
 
-            distutils_logger.info('Will execute the following command in with subprocess.Popen: \n{0}'.format(
-                  shell_command))
+            shell_command = "make {lib_type} && {mv} {source} {dest}".format(
+                lib_type=library_type,
+                mv=mv,
+                source=output_lib,
+                dest=os.path.join(output_dir, output_lib),
+            )
+            if conda_environment is not None:
+                if os.name == "nt":
+                    conda_activate = "activate {env} && "
+                else:
+                    conda_activate = 'conda shell.bash hook > temp_conda_hook && . temp_conda_hook && rm temp_conda_hook && conda activate {env} && '
+                distutils_logger.info(
+                    "Will attempt to conda activate the environment '{}' by executing {}".
+                    format(
+                        conda_environment, conda_activate.format(env=conda_environment)
+                    )
+                )
+                full_command = 'bash -c "{}"'.format(
+                    conda_activate.format(env=conda_environment) + shell_command
+                )
 
+            distutils_logger.info(
+                "Will execute the following command in with subprocess.Popen: \n{0}".format(
+                    full_command
+                )
+            )
+            distutils_logger.info("sources_path = {}".format(sources_path))
+            distutils_logger.info("sources_path content = {}".format(os.listdir(sources_path)))
 
-            make_process = subprocess.Popen(shell_command,
-                                            cwd=sources_path,
-                                            #~ stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE,
-                                            shell=True)
+            make_process = subprocess.Popen(
+                full_command,
+                cwd=sources_path,
+                # ~ stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+            )
             stdout, stderr = make_process.communicate()
             distutils_logger.debug(stdout)
             if stderr:
-                raise DistutilsSetupError('An ERROR occured while running the '
-                                          'Makefile for the {0} library. '
-                                          'Error status: {1}'.format(output_lib, stderr))
+                raise DistutilsSetupError(
+                    "An ERROR occured while running the "
+                    "Makefile for the {0} library. "
+                    "Error status: {1}".format(output_lib, stderr)
+                )
             # After making the library build the c library's python interface with the parent build_extension method
             super(specialized_build_ext, self).build_extension(ext)
 
-description = \
-"""
-This package is the source code linked to publication ...
 
-It provides an interface to the leaky integrator perception model, the
-data handling tools and also the modules used to perform the parameter
-search.
+description = "Leaky Integration underlying tactile perception interface for fitting and prediction"
 
-"""
 
-here = os.path.abspath(os.path.dirname(__file__))
 def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+    with codecs.open(os.path.join(PROJECT_ROOT, *parts), "r") as fp:
         return fp.read()
+
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-setuptools.setup(name = 'leakyIntegrator',
-        version = find_version('leakyIntegrator', '__init__.py'),
-        ext_modules = [extension1],
-        packages = setuptools.find_packages(),
-        cmdclass = {'build_ext': specialized_build_ext},
-        setup_requires=['numpy>=1.13'],
-        install_requires = ['matplotlib>=2',
-                            'numpy>=1.13',
-                            'sympy>=1.1.1',
-                            'scipy>=1',
-                            'six',
-                            'pandas>=0.21',
-                            'cma>=2.5',
-                            'mpmath>=1',
-                           ],
-        # metadata for upload to PyPI
-        author="Luciano Paz",
-        author_email="lpaz@sissa.it",
-        description=description,
-        license="MIT",
-        keywords="Leaky integrator perceptual decision making model, "
-                 "Stochastic dynamics, "
-                 "Ornstein Ulhenbeck process, "
-                 "Parametric model fit",
-        url="https://github.com/lucianopaz/leakyIntegrator",
-       )
+
+def get_requirements():
+    with codecs.open(REQUIREMENTS_FILE) as buff:
+        return buff.read().splitlines()
+
+
+def get_long_description():
+    with codecs.open(README_FILE, "rt") as buff:
+        return buff.read()
+
+
+setuptools.setup(
+    name="leakyIntegrator",
+    version=find_version("leakyIntegrator", "__init__.py"),
+    ext_modules=[extension1],
+    packages=setuptools.find_packages(),
+    cmdclass={"build_ext": specialized_build_ext},
+    setup_requires=["numpy>=1.13"],
+    install_requires=get_requirements(),
+    # metadata for upload to PyPI
+    author="Luciano Paz",
+    author_email="lpaz@sissa.it",
+    description=description,
+    long_description=get_long_description(),
+    license="MIT",
+    keywords="Leaky integrator perceptual decision making model, "
+    "Stochastic dynamics, "
+    "Ornstein Ulhenbeck process, "
+    "Parametric model fit",
+    url="https://github.com/tactile-perception-and-learning-lab/sensory_integration_time",
+)
