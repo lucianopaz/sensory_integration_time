35a0d28c01cdfa2701fcdd77e6920cf8c4716560
Luciano Paz <chulo1787@gmail.com>
2019-09-25 12:29:17 +0200

    Added a test to see what is failing with the c-api import

--- leakyIntegrator/tests/test_suite.py
+++ leakyIntegrator/tests/test_suite.py
@@ -6,6 +6,10 @@ class TestCApi():
     def test_module(self):
         assert "leaky_integral_calculator" in dir(leakyIntegrator)
 
+    def test_api_import(self):
+        from leakyIntegrator import leaky_integral_calculator
+        assert True
+
     @pytest.mark.parametrize(
         "method", ["leaky_integral_calculator", "lowergamma", "uppergamma"], ids=str
     )
